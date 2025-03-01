25ce66e818b717ff4bb1074d5d3ab889e3071919
Dhuruv Bansal <Dhuruvbansal53@gmail.com>
2025-03-01 11:42:14 +0530

    without infinite scroll

--- README.md
+++ README.md
@@ -1,36 +1,106 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+# Get Started
 
-## Getting Started
-
-First, run the development server:
-
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
+```
+npm i @tanstack/react-query
+npm i @tanstack/react-query-devtools
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+## Wrap App in provider
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+```
+"use client";
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
+export default function Queryrovider({ children }: { children: React.ReactNode }) {
+  const queryClient = new QueryClient();
 
-## Learn More
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+}
 
-To learn more about Next.js, take a look at the following resources:
+```
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+## useQuery
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
+```
+type queryFn = Promise<data>
+```
 
-## Deploy on Vercel
+```
+ const postQuery = useQuery({
+    queryKey: ["post"],
+    queryFn: queryFn,
+  });
+```
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+### Query stays fresh by giving staleTime in ms
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+```
+  const queryClient = new QueryClient({defaultOptions:{queries:{staleTime:1000}}});
+
+```
+
+```
+  const postQuery = useQuery({
+    queryKey: ["post"],
+    queryFn: queryFn,
+    staleTime: 1000 * 30,
+  });
+```
+
+### Refetch Interval
+
+```
+  const postQuery = useQuery({
+    queryKey: ["post"],
+    queryFn: queryFn,
+    refetchInterval: 1000,
+  });
+```
+
+### Enabled
+
+enable taked a boolean which fetch only after it true
+
+```
+  const getDataAfterPost = useQuery({
+    queryKey: ["data"],
+    queryFn: () => wait(1000).then(() => DATA),
+    enabled: postQuery.data !== undefined,
+  });
+```
+
+### PlaceHolderData
+
+```
+  const getPostByPage = useQuery({
+    queryKey: ["post", { page }],
+    placeholderData: keepPreviousData,
+    queryFn: () => wait(1000).then(() => POSTS[page]),
+  });
+```
+
+## useMutation
+
+```
+const newPostMutation = useMutation({
+    mutationFn: (post: string) => wait(1000).then(() => POSTS.push({ id: crypto.randomUUID(), post })),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["post"] });
+    },
+  });
+
+```
+
+use useClient hook to invalidate query
+
+```
+  const queryClient = useQueryClient();
+```
+
+## Invalidate Exact Query
+
+```
+ queryClient.invalidateQueries({ queryKey: ["post"], exact: true });
+
+```
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,85 @@
-import Image from "next/image";
+"use client";
+import { QueryClient, useMutation, useQuery, useQueryClient, keepPreviousData } from "@tanstack/react-query";
+import Link from "next/link";
+import { useState } from "react";
+
+function wait(ms: number) {
+  return new Promise((res) => setTimeout(res, ms));
+}
+
+const POSTS: Post[] = [
+  { id: 1, post: "post1" },
+  { id: 2, post: "post2" },
+  { id: 3, post: "post3" },
+  { id: 4, post: "post4" },
+  { id: 5, post: "post5" },
+  { id: 6, post: "post6" },
+  { id: 7, post: "pos7" },
+];
+
+const DATA = "this is data";
+
+type Post = { id: number | string; post: string };
+
+async function queryFn(queryClient: QueryClient): Promise<Post[]> {
+  await wait(1000);
+  queryClient.invalidateQueries({ queryKey: ["data"] });
+  return POSTS;
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [page, setPage] = useState<number>(1);
+  const queryClient = useQueryClient();
+  const postQuery = useQuery({
+    queryKey: ["post"],
+    queryFn: () => queryFn(queryClient),
+  });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const getDataAfterPost = useQuery({
+    queryKey: ["data"],
+    queryFn: () => wait(1000).then(() => DATA),
+    enabled: postQuery.data !== undefined,
+  });
+
+  const newPostMutation = useMutation({
+    mutationFn: (post: string) => wait(1000).then(() => POSTS.push({ id: crypto.randomUUID(), post })),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["post"], exact: true });
+      queryClient.setQueryData(["post"], [{ id: "test", post: "assc" }]);
+    },
+  });
+
+  const getPostByPage = useQuery({
+    queryKey: ["post", { page }],
+    placeholderData: keepPreviousData,
+    queryFn: () => wait(1000).then(() => POSTS[page]),
+  });
+
+  if (postQuery.isLoading) return <>Loading...</>;
+  if (postQuery.error) return <>{postQuery.error.message}</>;
+  return (
+    <div>
+      {Array.isArray(postQuery.data) &&
+        postQuery.data.map((item) => {
+          return <div key={item.id}>{item.post}</div>;
+        })}
+      <div>
+        {getDataAfterPost.isFetching ? "loading Data..." : getDataAfterPost.data}
+        <button className="p-2 bg-black text-white" onClick={() => newPostMutation.mutate("New Posts")}>
+          Add New
+        </button>
+        <Link href={"/test"}>test</Link>
+      </div>
+
+      <div>{getPostByPage.data?.post}</div>
+      <div className="flex gap-4">
+        <button disabled={getPostByPage.isPlaceholderData} onClick={() => setPage(page - 1)}>
+          Pre
+        </button>
+        <button disabled={getPostByPage.isPlaceholderData} onClick={() => setPage(page + 1)}>
+          Next
+        </button>
+      </div>
     </div>
   );
 }
